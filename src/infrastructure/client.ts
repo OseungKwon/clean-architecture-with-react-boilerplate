a9d9942b1d235a9e8147e0f18e58916644560713
OseungKwon <00osos@naver.com>
2024-06-23 17:54:44 +0900

    revert: CustomInstance 적용 롤백

--- src/infrastructure/client.ts
+++ src/infrastructure/client.ts
@@ -1,12 +1,5 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import axios from 'axios';
 
-export interface CustomInstance extends AxiosInstance {
-    get<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
-    post<T>(url: string, config?: AxiosRequestConfig, data?: unknown): Promise<T>;
-    put<T>(url: string, config?: AxiosRequestConfig, data?: unknown): Promise<T>;
-    delete<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
-}
-
-export const apiClient: CustomInstance = axios.create({
+export const apiClient = axios.create({
     baseURL: '/mock-test',
-}) as CustomInstance;
+});
