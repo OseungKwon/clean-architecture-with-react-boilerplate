54c6f67db0d4760d767c57dbfddd92795c9c704f
OseungKwon <00osos@naver.com>
2024-06-26 22:35:08 +0900

    feat: queryClient 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import SignInPage from './pages/sign-in/SignIn.page.tsx';
 import SignUpPage from './pages/sign-up/SignUp.page.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const router = createBrowserRouter([
     {
@@ -16,10 +17,11 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+    const queryClient = new QueryClient();
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
             <RouterProvider router={router} />
-        </>
+        </QueryClientProvider>
     );
 }
 
