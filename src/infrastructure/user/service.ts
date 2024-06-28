3060e2b4299aca073d454fc4549f86d10708fed6
OseungKwon <00osos@naver.com>
2024-06-28 23:33:13 +0900

    refactor: path 상대경로로 수정

--- src/infrastructure/user/service.ts
+++ src/infrastructure/user/service.ts
@@ -1,6 +1,6 @@
 import UserApi from './api.ts';
 import { UserRequest } from './dto.ts';
-import { dtoToUser } from '@/infrastructure/user/transform.ts';
+import { dtoToUser } from './transform.ts';
 async function getUser(request: UserRequest, api = UserApi) {
     const { data } = await api.getUser(request.identifier);
     return dtoToUser(data);
