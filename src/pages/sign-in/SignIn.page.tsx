8621da7acba918ac5a3ebd8d51c7dad74ac1b087
OseungKwon <00osos@naver.com>
2024-06-28 23:32:42 +0900

    feat: signin page

--- src/pages/sign-in/SignIn.page.tsx
+++ src/pages/sign-in/SignIn.page.tsx
@@ -1,5 +1,29 @@
+import useHandleSignIn from '@/application/sign-in/useHandleSignIn.ts';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type Inputs = {
+    nickname: string;
+    email: string;
+    password: string;
+};
+
 function SignInPage() {
-    return <div></div>;
+    const { onSingIn } = useHandleSignIn();
+    const { register, handleSubmit } = useForm<Inputs>();
+
+    const onSubmit: SubmitHandler<Inputs> = (data) => {
+        onSingIn(data);
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type={'email'} {...register('email')} />
+                <input type={'password'} {...register('password')} />
+                <button type={'submit'}>로그인</button>
+            </form>
+        </div>
+    );
 }
 
 export default SignInPage;
